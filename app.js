bfaf6ac9d7b563a95eb39d0b1042bfb529f8a565
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-06-21 23:52:20 +0200

    Added workflow

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
+
+app.get('/2', (req, res) => {
+    res.send('2nd endpoint.');
+  });
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
