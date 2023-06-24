d9d41ed2fadc3dfd1b5656e7064147b470762b06
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-06-24 12:08:27 +0200

    implemented google oath2 with passport

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 import express from "express";
-import { config } from "dotenv";
 import logger from "morgan";
+import { config } from "dotenv";
+import passport from 'passport';
+import errorHandler from "./middleware/errorHandler.js";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import connectSqlite from "connect-sqlite3";
+import csrf from "csurf";
+import path from "path";
+import { fileURLToPath } from 'url';
+import { ensureLoggedIn } from "connect-ensure-login";
 
+import authRouter from "./routes/auth.route.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(`filename: ${__filename}, dirname: ${__dirname}`);
 config();
 const app = express();
-app.use(express.json());
+
+
+const SQLiteStore = connectSqlite(session);
+
 if (["development", "production"].includes(process.env.NODE_ENV)) {
-    app.use(logger("dev"));
-  }
+  app.use(logger("dev"));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(errorHandler);
+app.use(session({
+  secret: 'the ultimate secret',
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(csrf());
+app.use(passport.authenticate('session'));
+
+app.use(function(req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+passport.initialize();
+
+app.use('/auth', authRouter);
+app.use('/home', express.static(__dirname + '/public'));
+app.use('/', ensureLoggedIn('/home'), express.static(__dirname + '/protected'))
 
 export default app;
---
+++ routes/auth.route.js
@@ -0,0 +1,58 @@
+import passport from "passport";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import { Router } from "express";
+import passportGoogle from "../services/passport/passport-google.js";
+
+const authRouter = Router()
+
+authRouter.get('/login', function(req, res, next) {
+  res.redirect('/');
+});
+
+authRouter.get('/images/logo.svg', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../public/images/logo.png'));
+});
+
+authRouter.get('/images/logo.svg', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../public/images/logo.png'));
+});
+
+authRouter.get('/css/style.css', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/../public/css/style.css'));
+});
+
+authRouter.get('/login/federated/google', passport.authenticate('google'));
+
+authRouter.get('/oauth2/redirect/google', passport.authenticate('google', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/auth/login',
+  keepSessionInfo: true
+}));
+
+authRouter.get('/user', function(req, res, next) {
+  if (req.user) {
+    res.status(200).json({
+      status: "success",
+      user: req.user,
+    });
+  } else {
+    res.status(401).json({
+      status: "unauthorized",
+      message: "You are not logged in."
+    });
+  }
+});
+
+authRouter.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/home');
+  });
+});
+
+export default authRouter;
