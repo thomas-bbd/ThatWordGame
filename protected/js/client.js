104d594877b583b0fdc204a7bb1c45d81b1564be
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-06-27 20:46:50 +0200

    back to ws

--- protected/js/client.js
+++ protected/js/client.js
@@ -7,10 +7,9 @@ import { ClientPlayer } from "./player-state-machine.js"
 
 let client = null
 
-// const wsURL = window.location.host.includes("localhost") ? `ws://${window.location.host}/` : `ws://${window.location.host}/`;
-// console.log(`ws url ${wsURL}`)
-// const socket = new WebSocket(wsURL);
-const socket = io();
+const wsURL = window.location.host.includes("localhost") ? `ws://${window.location.host}/` : `ws://${window.location.host}/`;
+console.log(`ws url ${wsURL}`)
+const socket = new WebSocket(wsURL);
 
 async function fetchName() {
     let response = await fetch("/auth/user");
@@ -19,7 +18,9 @@ async function fetchName() {
     return data.user.name;
 }
 
-socket.on("connect", () => {
+
+socket.onopen = () => {
+    // check if url containers the parameter create or join
     let urlParams = new URLSearchParams(window.location.search);
     let create = urlParams.get('create');
     let join = urlParams.get('join');
@@ -30,12 +31,6 @@ socket.on("connect", () => {
     } else if (create == null && join == null) {
         window.location = "/home";
     }
-  });
-
-
-socket.onopen = () => {
-    // check if url containers the parameter create or join
-    
 };
 
 async function createPlayer() {
