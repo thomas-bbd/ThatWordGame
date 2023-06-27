7c516ea5a5b86432128b9dba11b0568665b305ff
Thomas Stevens <thomas.stevens@vcontractor.co.za>
2023-06-27 16:47:30 +0200

    test

--- protected/js/client.js
+++ protected/js/client.js
@@ -7,7 +7,7 @@ import { ClientPlayer } from "./player-state-machine.js"
 
 let client = null
 
-const wsURL = window.location.host.includes("localhost") ? `ws://${window.location.host}/` : `ws://${window.location.host}/`;
+const wsURL = window.location.host.includes("localhost") ? `ws://${window.location.host}/` : `ws://${window.location.host}:5000/`;
 const socket = new WebSocket(wsURL);
 
 async function fetchName() {
